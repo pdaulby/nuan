2879bad665d613c4896150b0e39e901596fb5daa
pdaulby <p.daulby@hotmail.com>
2022-03-08 11:03:22 +0000

    figure out mobx reactions and how to use them to trigger input changes

--- src/header/Topic.tsx
+++ src/header/Topic.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import { reaction } from "mobx";
+import React, { useState } from "react";
 import { EditTextarea } from 'react-edit-text';
 import 'react-edit-text/dist/index.css';
 import store from "../store/Store";
 
 const Topic: React.FC = () => {
+    const [topic, setTopic] = useState("");
+    reaction(()=>store.topic, setTopic);
+
     return (
         <div className="topic">
         <EditTextarea 
             placeholder="Type topic here." 
             className="edit-topic"
-            defaultValue={store.topic} //I should not be using this library
-            onSave={(save) => store.updateTopic(save.value)}
+            value={topic}
+            onChange={store.updateTopic}
         />
         </div>
     )
